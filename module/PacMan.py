997e87c408e5282c882ba1cc5b096c7146d8caa9
Jesse <jessie97013@gmail.com>
2021-12-23 00:58:31 -0800

    PacMan base setting

--- module/PacMan.py
+++ module/PacMan.py
@@ -7,5 +7,32 @@ from setting import *
 
 
 class PacMan(pygame.sprite.Sprite):
+    def __init__(self, color=YELLOW):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([20, 20])
+        self.color = color
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 300
+        self.rect.centery = 400
 
-    super.__init__()
+    def update(self):
+        if motion == "UP":
+            self.rect.centery -= 10
+        elif motion == "DOWN":
+            self.rect.centery += 10
+        elif motion == "LEFT":
+            self.rect.centerx -= 10
+        elif motion == "RIGHT":
+            self.rect.centerx += 10
+
+    @property
+    def player_data(self):
+        return {
+            "tyep": "rect",
+            "name": "pac-man",
+            "x": self.rect.x,
+            "y": self.rect.y,
+            "width": self.rect.width,
+            "height": self.rect.height,
+            "color": self.color
+        }
